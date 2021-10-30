3390509bb732212f73031f1f30b0091b01559b1a
alexeykuz-sys <alexeykuz@gmail.com>
2021-10-30 11:10:26 +0100

    added meal items

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,8 +1,15 @@
+import CartIcon from "../Cart/CartIcon";
+import classes from './HeaderCartButton.module.css';
+
 const HeaderCartButton = props =>{
-    return <button>
-        <span></span>
-        <span></span>
-        <span></span>
+    return <button className={classes.button}>
+        <span className={classes.icon}>
+            <CartIcon/>
+        </span>
+        <span>Your cart</span>
+        <span className={classes.badge}>
+            £3
+        </span>
     </button>
 };
 
